4fe9bd8f9cdec24c225fd83eb2700e3fcc40ef53
Egor Еtrofimov <martynenkoa01@gmail.com>
2022-08-17 20:37:56 +0300

    final version

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Вводим массив через консоль
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine($"Введите  массив [{index}]:");
+        int N = Convert.ToInt32(Console.ReadLine());
+        array[index] = N;
+        index++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int index = 0;
+    while (index < count)
+
+
+        
+        {
+            Console.Write(arr[index] + " ");
+            index++;
+        }
+}
+
+    int[] array = new int[8];
+    int count = array.Length;
+
+    FillArray(array);
+    PrintArray(array);
+
+
